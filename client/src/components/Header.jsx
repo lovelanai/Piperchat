d31e4f79b155ecf40e1c5ff00d9dac13a2d0ba51
matsegard <matsegard@outlook.com>
2022-05-06 15:27:48 +0200

    fixed name bug on socket connect
    Co-authored-by: Love Lanai <lovelanai@users.noreply.github.com>

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,13 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
+import piedpiper from "../assets/img/piedpiper.png";
 
 function Header() {
   return (
     <div className="header">
-      <Link to="/">
-        <div>
-          <h1>Stranger Danger</h1>
-        </div>
-      </Link>
+      <div style={{ position: "relative" }}>
+        <Link to="/">pied piper</Link>
+      </div>
     </div>
   );
 }
