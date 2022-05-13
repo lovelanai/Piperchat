b85a41c970673884176d258124dc7f8da84c6e48
lovelanai <94110810+lovelanai@users.noreply.github.com>
2022-05-13 13:34:34 +0200

    styling
    Co-authored-by: Erik Matsegård <matsegard@outlook.com>

--- client/src/components/Roomnavbar.jsx
+++ client/src/components/Roomnavbar.jsx
@@ -1,30 +1,73 @@
 import "./Roomnavbar.css";
 import { useUser } from "../context/ContextUser";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function Roomnavbar() {
   const { newRoom, joinRoom, rooms, currentRoom, joinAvalibleRoom } = useUser();
 
   return (
     <div className="Room-nav-container">
-      <div className="containerRooms">
-        <h2>Chatrooms</h2>
-        {rooms.map((room, index) => (
-          <div key={index}>
-            <button
-              className="roomsButtons"
-              disabled={currentRoom === room}
-              value={room || ""}
-              onClick={joinAvalibleRoom}
-            >
-              {room}
+      <div className="desktop-nav">
+        {" "}
+        <h2>Rooms</h2>
+        <div className="containerRooms">
+          {rooms.map((room, index) => (
+            <div key={index}>
+              <button
+                className="roomsButtons"
+                disabled={currentRoom === room}
+                value={room || ""}
+                onClick={joinAvalibleRoom}
+              >
+                {room}
+              </button>
+            </div>
+          ))}
+          <div>
+            <button onClick={newRoom} className="roomsButtons">
+              <span>Skapa nytt rum</span>{" "}
+              <AiOutlinePlusCircle style={{ marginLeft: "0.2rem" }} />
             </button>
           </div>
-        ))}
-        <button onClick={newRoom} className="newRoom">
-          <span>Skapa nytt rum</span>{" "}
-          <AiOutlinePlusCircle style={{ marginLeft: "0.2rem" }} />
-        </button>
+        </div>
+      </div>
+      <div className="mobile-nav">
+        <Navbar
+          style={{ justifyContent: "center" }}
+          collapseOnSelect
+          expand="lg"
+          variant="dark"
+        >
+          <Navbar.Brand>
+            <h2>Chatrooms</h2>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <div className="containerRooms">
+                {rooms.map((room, index) => (
+                  <div key={index}>
+                    <button
+                      className="roomsButtons"
+                      disabled={currentRoom === room}
+                      value={room || ""}
+                      onClick={joinAvalibleRoom}
+                    >
+                      {room}
+                    </button>
+                  </div>
+                ))}
+                <div>
+                  <button onClick={newRoom} className="roomsButtons">
+                    <span>Skapa nytt rum</span>{" "}
+                    <AiOutlinePlusCircle style={{ marginLeft: "0.2rem" }} />
+                  </button>
+                </div>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     </div>
   );
